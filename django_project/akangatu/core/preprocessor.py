0b503fe5255509e58bad810109cb5cbde683dc23
fsalhani <fduartesalhani@gmail.com>
2018-12-27 13:51:05 -0200

    modify reshape data

--- django_project/akangatu/core/preprocessor.py
+++ django_project/akangatu/core/preprocessor.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+
+MNIST_WIDTH = 28
+MNIST_HEIGHT = 28
+COLOR_SCALE = 255
+
 class Preprocessor:
-	def reshape_data(request):
-		pixel_array = np.array(request)
-		return pixel_array.reshape(-1, 1, 28, 28) / 255.0
+	def reshape_data(pixel_array):
+		return pixel_array.reshape(1, 1, MNIST_WIDTH, MNIST_HEIGHT) / COLOR_SCALE
